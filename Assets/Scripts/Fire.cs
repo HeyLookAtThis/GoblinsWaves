371c1e3bbe827089d59ff96b3343c4943fe06111
HeyLookAtThis <r.a.yamov@yandex.ru>
2023-06-30 11:36:21 +0500

    30.06 11:36

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Fire))]
+[RequireComponent(typeof(Light))]
 public class Fire : MonoBehaviour
 {
     private Light _light;
+    private Coroutine _intensityChangerCoroutine;
 
     private void Awake()
     {
@@ -14,17 +15,31 @@ public class Fire : MonoBehaviour
 
     private void OnEnable()
     {
-        StartCoroutine(IntensityChanger());
+        BeginChangeIntensity();
     }
 
     private void OnDisable()
     {
-        StopCoroutine(IntensityChanger());
+        EndChangeIntencity();
+    }
+
+    private void BeginChangeIntensity()
+    {
+        if (_intensityChangerCoroutine != null)
+            StopCoroutine(_intensityChangerCoroutine);
+
+        _intensityChangerCoroutine = StartCoroutine(IntensityChanger());
+    }
+
+    private void EndChangeIntencity()
+    {
+        if (_intensityChangerCoroutine != null)
+            StopCoroutine(_intensityChangerCoroutine);
     }
 
     private IEnumerator IntensityChanger()
     {
-        float minIntensity = 2.7f;
+        float minIntensity = 2.5f;
         float maxIntensity = 3;
 
         float seconds = 0.1f;
